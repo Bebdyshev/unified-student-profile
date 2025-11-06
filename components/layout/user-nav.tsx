472fcfccf37c4a24b61b0d180e7745f499f7d53d
Bebdyshev <berdyshev_k1004@akb.nis.edu.kz>
2025-11-06 17:27:32 +0500

    fix: update settings link in user navigation to point to admin settings

--- components/layout/user-nav.tsx
+++ components/layout/user-nav.tsx
@@ -31,7 +31,7 @@ export function UserNav() {
           </Link>
         </DropdownMenuItem>
         <DropdownMenuItem asChild>
-          <Link href="/dashboard/settings">
+          <Link href="/admin/settings">
             <Settings className="mr-2 h-4 w-4" />
             <span>Settings</span>
           </Link>
